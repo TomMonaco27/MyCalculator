ecf365ccf356de23989b72496f3e8117ccddb816
TomMonaco27 <donotwritetome27@gmail.com>
2020-06-03 10:18:59 +0300

    Initial Commit

---
+++ MyCalculator_New/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCalculator_New
+//
+//  Created by mac on 03.06.2020.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
